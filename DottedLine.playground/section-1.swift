be7f967c22a784dd5dc93f6be648f95283295902
elkraneo <cristian@metodowhite.com>
2014-11-05 12:54:44 +0100

    expanded example on InverImage
    refactoring

--- DottedLine.playground/section-1.swift
+++ DottedLine.playground/section-1.swift
@@ -1,8 +1,6 @@
 // Playground - noun: a place where people can play
 
 import UIKit
-
-import UIKit
 import XCPlayground
 
 let path = UIBezierPath()
--- InvertImage.playground/section-1.swift
+++ InvertImage.playground/section-1.swift
@@ -1,6 +1,7 @@
 // Playground - noun: a place where people can play
 
 import UIKit
+import XCPlayground
 
 let positiveImage = UIImage(named: "imageTexture")
 
@@ -15,5 +16,25 @@ CGContextSetBlendMode(UIGraphicsGetCurrentContext(), kCGBlendModeSourceOut);
 CGContextSetFillColorWithColor(UIGraphicsGetCurrentContext(), UIColor.blackColor().CGColor);
 CGContextFillRect(UIGraphicsGetCurrentContext(), rect);
 
-let returnImage = UIGraphicsGetImageFromCurrentImageContext();
+XCPCaptureValue("negative image", UIGraphicsGetImageFromCurrentImageContext())
+let negativeImage = UIGraphicsGetImageFromCurrentImageContext()
+
 UIGraphicsEndImageContext();
+
+
+//–– Test with extra size in result image
+
+let rect2 = CGRect(x: 800, y: 500, width: positiveImage!.size.width, height: positiveImage!.size.height)
+let rect3 = CGRect(x: 0, y: 0, width: 2000, height: 2000)
+
+UIGraphicsBeginImageContext(rect3.size)
+
+positiveImage?.drawInRect(rect2)
+
+CGContextSetBlendMode(UIGraphicsGetCurrentContext(), kCGBlendModeSourceOut);
+
+CGContextSetFillColorWithColor(UIGraphicsGetCurrentContext(), UIColor.blackColor().CGColor);
+CGContextFillRect(UIGraphicsGetCurrentContext(), rect3);
+
+XCPCaptureValue("image with extra size", UIGraphicsGetImageFromCurrentImageContext())
+
