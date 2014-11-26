c8aad35e1c03871b529066e145b5537ecc27ec26
elkraneo <cristian@metodowhite.com>
2014-11-27 00:52:07 +0100

    fixed RotateView

--- RotateView.playground/section-1.swift
+++ RotateView.playground/section-1.swift
@@ -6,12 +6,23 @@
 import UIKit
 import XCPlayground
 
-let leView = UIView(frame: CGRect(x: 10, y: 10, width: 200, height: 200))
-leView.backgroundColor = UIColor.redColor()
+//https://developer.apple.com/library/ios/releasenotes/DeveloperTools/RN-Xcode/Chapters/xc6_release_notes.html
+//iOS Playgrounds now support displaying animated views with the XCPShowView() XCPlayground API. This capability is disabled by default; it can be enabled by checking the "Run in Full Simulator" setting in the Playground Settings inspector.
+//When the capability is enabled, running the playground causes the iOS Simulator application to launch and run the playground in the full simulator. This capability is also required for other functionality that fails without the full simulator, such as NSURLConnection http requests. Running in the full iOS Simulator is slower than running in the default mode. (18282806)
+
+
+let containerView = UIView(frame: CGRect(x: 0, y: 0, width: 500, height: 500))
+containerView.backgroundColor = UIColor.grayColor()
+
+let aView = UIView(frame: CGRect(x: 10, y: 10, width: 200, height: 200))
+aView.backgroundColor = UIColor.darkGrayColor()
+
+containerView.addSubview(aView)
+
 
 UIView.animateWithDuration(1, animations: { () -> Void in
-    leView.transform = CGAffineTransformMakeRotation(CGFloat(1.3 * M_2_PI))
+    aView.transform = CGAffineTransformMakeRotation(CGFloat(1.3 * M_PI))
 })
 
 
-XCPShowView("leView", leView)
+XCPShowView("containerView", containerView)
