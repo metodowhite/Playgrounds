8d2eec79d9976790899c4fe3b78a04dbd221ca57
elkraneo <cristian@metodowhite.com>
2015-05-28 13:27:04 +0200

    deleted "external" playgrounds

--- external/lynda/Swift Essential Training/Generics_starter.playground/section-1.swift
+++
@@ -1,7 +0,0 @@
-// Playground - Generics
-
-import UIKit
-
-class SimpleClass {
-    var singleProperty : String = "A string"
-}
--- external/lynda/Swift Essential Training/Operators_starter.playground/section-1.swift
+++
@@ -1,9 +0,0 @@
-// Playground - Operators
-
-import UIKit
-
-var a = 5
-var b = 50
-var c = 11.38
-var highScore = 500000
-
